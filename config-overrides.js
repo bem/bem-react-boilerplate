715cdb04fcd46c0e8306381efcb86702ff02e7ac
awinogradov <winogradovaa@gmail.com>
2018-04-11 19:21:40 +0300

    use package

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,9 @@
-const rewirePlatformBundles = require('./tools/rewirePlatformBundles');
+const rewireBemSets = require('rewire-bem-sets');
 
 module.exports = function override(config, env) {
   // use any rewires here ;-) You are welcome!
 
-  return rewirePlatformBundles({
+  return rewireBemSets({
     techs: ['js', 'css']
   })(config, env);
 }
