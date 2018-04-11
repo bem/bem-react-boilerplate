84c392576dabd056624c31daea7c80e3719d4529
awinogradov <winogradovaa@gmail.com>
2018-04-11 14:34:20 +0300

    clean comments

--- config-overrides.js
+++ config-overrides.js
@@ -3,9 +3,6 @@ const rewirePlatformBundles = require('./tools/rewirePlatformBundles');
 module.exports = function override(config, env) {
   // use any rewires here ;-) You are welcome!
 
-  // TODO:
-  // 1. Do we need support env? Now it works fine in DEV and PROD
-  // 2. Change current platform by process.env.PLATFORM
   return rewirePlatformBundles({
     techs: ['js', 'css'],
     platforms: ['desktop', 'touch']
