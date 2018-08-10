d2b5046e05a1c614b7acaa4d0e1c8e755d897340
Evgeniy Baranov <master@kompolom.ru>
2018-08-10 11:59:33 +0300

    Fix jest fall
    * adds testURL to config
    * replace svg modules to empty mock

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testURL : 'http://localhost',
   testPathIgnorePatterns : ['node_modules'],
   moduleFileExtensions : ['ts', 'tsx', 'js'],
   transform : {
@@ -8,6 +9,6 @@ module.exports = {
       '**/src/**/*.test.+(ts|tsx)'
   ],
   moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/__mocks__/style.ts'
+    '\\.(css|less|svg)$': '<rootDir>/__mocks__/style.ts'
   }
 };
