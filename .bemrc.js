d4ec6e5ec59e0984c7b35b32d5e036af5722adf8
Vladimir Grinenko <i@tadatuta.com>
2018-08-11 14:40:35 +0300

    .bemrc: Cleanup

--- .bemrc.js
+++ .bemrc.js
@@ -1,27 +1,4 @@
 module.exports = {
-  levels: [
-    {
-      layer: 'common',
-      path: 'src/blocks'
-    },
-    {
-      layer: 'desktop',
-      path: 'src/desktop'
-    },
-    {
-      layer: 'touch',
-      path: 'src/touch'
-    }
-  ].map(level => Object.assign({
-    schemeOptions: 'react',
-    naming: 'react',
-    scheme: 'nested' }, level)
-  ),
-  // remove sets to build one universal bundle for index.html
-  sets: {
-    desktop: 'common desktop',
-    touch: 'common touch'
-  },
   modules: {
     'bem-tools': {
       plugins: {
